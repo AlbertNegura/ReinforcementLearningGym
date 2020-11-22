9ed16410d73f8e2c40b548c3ad38dc2d355c1d4c
Albert Negura <Albert.Negura@Gmail.com>
2020-11-22 22:42:50 +0100

    Basic variables and imports

--- gymexample.py
+++ gymexample.py
@@ -1,15 +1,20 @@
 import gym
+import numpy as np
+import random
+import time
+import matplotlib.pyplot as plt
+
 
 slow = True
 
-# env = gym.make("MountainCar-v0")
+env = gym.make("MountainCar-v0")
 
 gym.envs.register(id='MountainCarMyEasyVersion-v0',
                   entry_point='gym.envs.classic_control:MountainCarEnv',
                   max_episode_steps=100000,  # MountainCar-v0 uses 200
                   )
 
-env = gym.make('MountainCarMyEasyVersion-v0')
+#env = gym.make('MountainCarMyEasyVersion-v0')
 
 for _ in range(10):
     observation = env.reset()
@@ -24,3 +29,12 @@ for _ in range(10):
         if slow: print(reward)
         if slow: print(done)
     print(f"Episode finished after {timesteps} timesteps.")
+
+action_sapce = env.action_space
+epsilon = 1
+learning_rate = 0.001
+discount_rate = 0.999
+
+Q_values = np.zeros((20, 20, action_sapce.n))
+max_episode_steps = 10000
+env._max_episode_steps=max_episode_steps
