7ae8a775e7994b68690688e9ebf39504881c4931
Albert Negura <Albert.Negura@Gmail.com>
2020-11-24 12:23:42 +0100

    Hardly working

--- gymexample.py
+++ gymexample.py
@@ -5,12 +5,13 @@ import random
 import time
 import argparse as ap
 
-alpha = 0.05
+alpha = 0.1
 gamma = 0.9
 epsilon = 1.0
 epsilon_min = 0.005
 decay_factor = 500
-max_episode_steps = 200
+max_episode_steps = 10000
+RENDER = False
 
 
 def linearize(env, i, bins):
@@ -76,7 +77,7 @@ class SARSA:
         discretized_obs = discretize(obs, self.pos, self.vel)
         discretized_next_obs = discretize(next_obs, self.pos, self.vel)
         next_action = self.policy[discretized_next_obs]
-        action_values = reward + gamma * np.max(self.action_state_vals[discretized_next_obs]) - self.action_state_vals[discretized_obs][action]
+        action_values = reward + gamma * self.action_state_vals[discretized_next_obs][next_action] - self.action_state_vals[discretized_obs][action]
         self.action_state_vals[discretized_obs][action] += alpha * action_values
         self.policy[discretized_obs] = np.argmax(self.action_state_vals[discretized_obs])
 
@@ -98,8 +99,10 @@ class Agent:
                 self.agent.learn(obs, action, reward, next_obs)
                 obs = next_obs
                 total += reward
+                if reward > best_reward and RENDER:
+                    self.env.render()
             best_reward = max(best_reward, total)
-            #print('Episode: {}, Reward: {}, Best_reward: {}'.format(ep, total, best_reward))
+            print('Episode: {}, Reward: {}, Best_reward: {}'.format(ep, total, best_reward))
         return np.argmax(self.agent.action_state_vals, axis=2)
 
     def test(self, policy):
@@ -116,24 +119,29 @@ class Agent:
 if __name__ == "__main__":
     slow = True
 
-    env = gym.make("MountainCar-v0")
+    #env = gym.make("MountainCar-v0")
 
-    #gym.envs.register(id='MountainCarMyEasyVersion-v0',entry_point='gym.envs.classic_control:MountainCarEnv',max_episode_steps=100000,)  # MountainCar-v0 uses 200
-    #env = gym.make('MountainCarMyEasyVersion-v0')
-
-
-    max_episodes = np.arange(10000, 100000, 10000, dtype=np.int32)
-    bins = np.arange(10, 100, 5, dtype=np.int32)
+    gym.envs.register(id='MountainCarMyEasyVersion-v0',entry_point='gym.envs.classic_control:MountainCarEnv',max_episode_steps=max_episode_steps,)  # MountainCar-v0 uses 200
+    env = gym.make('MountainCarMyEasyVersion-v0')
 
 
-    agent = QLearning(env, bins[0], max_episodes[0], epsilon)
-    handler = Agent(env, agent)
-    policy = handler.train()
+    max_episodes = np.arange(50000, 100000, 10000, dtype=np.int32)
+    bins = np.arange(20, 50, 5, dtype=np.int32)
+
+    agent = []
+
+    agent.append(QLearning(env, bins[0], max_episodes[0], epsilon))
+    agent.append(SARSA(env, bins[0], max_episodes[0], epsilon))
+
+    handler = [Agent(env, agent[i]) for i in range(len(agent))]
+    policy = [handler[i].train() for i in range(len(handler))]
 
     #output_dir = './q_output'
     #env = gym.wrappers.Monitor(env, output_dir, force=True)
-    for _ in range(10):
-        handler.test(policy)
+    for i in range(len(handler)):
+        for _ in range(1000):
+            env.render()
+            [handler[i].test(policy)]
 
 
     env.close()
